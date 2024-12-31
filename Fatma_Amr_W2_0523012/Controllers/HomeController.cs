237d287aedcab3bef2e1fb76a42abb400b718323
FatmaAmr0 <Fatema.Amr@ELSEWEDY-SCHOOL.LOCAL>
2024-12-31 12:24:48 +0200

    firstCommit

---
+++ Fatma_Amr_W2_0523012/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Fatma_Amr_W2_0523012.Models;
+
+namespace Fatma_Amr_W2_0523012.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
---
+++ Fatma_Amr_W2_0523012/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Fatma_Amr_W2_0523012.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
